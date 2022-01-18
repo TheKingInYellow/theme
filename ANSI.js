a8936d9c1f7deb1a9ffb846b0834fed95d43bfbe
TheKingInYellow <dwhitney@utexas.edu>
2022-01-18 13:09:44 -0600

    First commit

---
+++ ANSI.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#000000", "#AA0000", "#00AA00", "#AAAA00", "#0000AA", "#AA00AA", "#00AAAA", "#AAAAAA", "#555555", "#FF5555", "#55FF55", "#FFFF55", "#5555FF", "#FF55FF", "#55FFFF", "#FFFFFF"]);
+t.prefs_.set('foreground-color', "#AAAAAA");
+t.prefs_.set('background-color', "#000000");
+t.prefs_.set('cursor-color', 'rgba(165,162,162,0.5)');
